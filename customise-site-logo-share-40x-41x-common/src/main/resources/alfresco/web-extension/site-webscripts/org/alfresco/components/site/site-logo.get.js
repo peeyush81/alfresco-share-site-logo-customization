f99dcd08f12da6faedb844ed76b3aca6e18c4fcf
douglascrp <douglascrp@28422569-abf0-3e42-b055-8fc4dff18062>
2013-10-23 11:59:04 +0000

    Fix error reading the site's "properties" property when the site doesn't have it. This can happen when the site is exported from a repository that doesn't have the addon installed and imported in a new repository that has the addon installed.

--- customise-site-logo-share-40x-41x-common/src/main/resources/alfresco/web-extension/site-webscripts/org/alfresco/components/site/site-logo.get.js
+++ customise-site-logo-share-40x-41x-common/src/main/resources/alfresco/web-extension/site-webscripts/org/alfresco/components/site/site-logo.get.js
@@ -4,11 +4,14 @@ function main()
 	var siteId = page.url.templateArgs.site || "";
 	if (siteId !== "") {
 		var p = sitedata.getPage("site/" + siteId + "/dashboard");
-	    var siteLogo = p.properties.siteLogo;
-	    model.siteLogo = siteLogo;
-	    if (siteLogo !== null && typeof siteLogo !== "undefined" && siteLogo.length !== 0) {
-	        logo = siteLogo;
-	    }
+	    var siteProperties = p.properties;
+		if (siteProperties 	!== null ) {
+			var siteLogo = p.properties.siteLogo;
+			model.siteLogo = siteLogo;
+			if (siteLogo !== null && typeof siteLogo !== "undefined" && siteLogo.length !== 0) {
+				logo = siteLogo;
+			}
+		}
 	}
 	
 	model.logo = logo;
